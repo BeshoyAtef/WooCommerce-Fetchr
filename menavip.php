476a96df0c588fefa7fb83666cca690af1f5644c
Asaad Abdo <as3ad_abdo@yahoo.com>
2015-06-20 16:54:43 +0400

    first commit

---
+++ menavip.php
@@ -0,0 +1,366 @@
+<?php ob_start();
+/**
+ * Plugin Name: WooCommerce With Menavip
+ * Plugin URI: http://menavip.com
+ * Description: A plugin to integrate menavip shipping with woo commerce.
+ * Author: fetchr
+ * Author URI: http://www.fetchr.us
+ */
+add_action('admin_menu', 'test_plugin_setup_menu');
+//define(ERP_URL);
+function test_plugin_setup_menu(){
+    add_menu_page( 'MENAVIP ERP Integration', 'Mena Vip', 'manage_options', 'test-plugin', 'mena_setting_page' );
+    add_action( 'admin_init', 'register_setting_options' );
+}
+
+function register_setting_options()
+{
+    register_setting( 'mena-settings-group', 'mena_merchant_name' );
+    register_setting( 'mena-settings-group', 'mena_merchant_password' );
+    register_setting( 'mena-settings-group', 'mena_pickup_location' );
+    register_setting( 'mena-settings-group', 'mena_fetch_status');
+    register_setting( 'mena-settings-group', 'mena_servcie_type');
+    register_setting( 'mena-settings-group', 'mena_merchant_phone_number');
+    register_setting( 'mena-settings-group', 'erp_user_name');
+    register_setting( 'mena-settings-group', 'mena_is_production');
+    register_setting( 'mena-settings-group', 'mena_is_uae_only');
+
+}
+
+function mena_setting_page() {
+    ?>
+    <div class="wrap">
+        <h2> Mena Vip Integration</h2>
+
+        <form method="post" action="options.php">
+            <?php settings_fields( 'mena-settings-group' ); ?>
+            <?php do_settings_sections( 'mena-settings-group' ); ?>
+            <table class="form-table">
+
+
+                <tr valign="top">
+                    <th scope="row">ERP Username/merchant for fulfilment</th>
+                    <td><input type="text" name="erp_user_name" value="<?php echo esc_attr(get_option('erp_user_name')); ?>" /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">MENAVIP User Name</th>
+                    <td><input type="text" name="mena_merchant_name" value="<?php echo esc_attr( get_option('mena_merchant_name') ); ?>" /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">MENAVIP User Password</th>
+                    <td><input type="text" name="mena_merchant_password" value="<?php echo esc_attr( get_option('mena_merchant_password') ); ?>" /></td>
+                </tr>
+
+
+                <tr valign="top">
+                    <th scope="row">Merchant registered phone number</th>
+                    <td><input type="text" name="mena_merchant_phone_number" value="<?php echo esc_attr( get_option('mena_merchant_phone_number') ); ?>" /></td>
+                </tr>
+
+
+                <tr valign="top">
+                    <th scope="row">Service Type</th>
+                    <td>
+
+                        <select name="mena_servcie_type" >
+                            <option value="delivery" <?php if (get_option('mena_servcie_type') == "delivery"): ?> selected="selected" <?php endif; ?>>Delivery Only</option>
+                            <option value="fulfil_delivery" <?php if (get_option('mena_servcie_type') == "fulfil_delivery"): ?> selected="selected" <?php endif; ?>> Fulfilment + Delivery</option>
+                        </select>
+
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Live Server</th>
+                    <td>
+                        <input name="mena_is_production" type="checkbox" value="1" <?php if (get_option('mena_is_production') == "1"): ?> checked="checked" <?php endif; ?> />
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">For UAE Only</th>
+                    <td>
+                        <input name="mena_is_uae_only" type="checkbox" value="1" <?php if (get_option('mena_is_uae_only') == "1"): ?> checked="checked" <?php endif; ?> />
+                    </td>
+                </tr>
+
+
+            </table>
+
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php 			     hit_mena_api();
+
+}
+// setting a  cron to run hourly
+add_action( 'wp', 'setup_schedule_event' );
+function setup_schedule_event()
+{
+    if ( ! wp_next_scheduled( 'prefix_hourly_event' ) )
+    {
+        wp_schedule_event( time(), 'hourly', 'prefix_hourly_event');
+    }
+}
+
+add_action( 'prefix_hourly_event', 'hit_mena_api' );
+function hit_mena_api()
+{
+    $server_url = "http://dev.menavip.com/";
+
+    if (get_option('mena_is_production') == "1")
+    {
+        $server_url = "http://menavip.com/";
+    }
+
+    //echo $server_url;
+
+    if ( get_option("mena_fetch_status") )
+    {
+        $where = array( get_option("mena_fetch_status") );
+    }
+    else
+    {
+        $where = array("wc-processing");
+        //$where = array_keys( wc_get_order_statuses() );
+    }
+    $orders = get_posts( array(
+            'post_type'   => 'shop_order',
+            'post_status' => $where
+        )
+    );
+    //var_dump($orders);
+    //exit;
+    foreach ($orders as $order)
+    {
+        $shipping_country = get_post_meta($order->ID,'_shipping_country',true);
+        $order_wc = new WC_Order( $order->ID );
+        //  Check if the plugin for UAE only
+        if (get_option('mena_is_uae_only') == "1" &&  $shipping_country !=="AE")
+            continue;
+
+        if( get_option('mena_servcie_type') == "fulfil_delivery")
+        {
+            // fulfilment + delivery
+            $products = $order_wc->get_items();
+            menavip_fulfil_delivery ($order,$order_wc,$products,$server_url);
+        }
+        else
+        {
+            // delivery only
+            menavip_delivery_only ($order,$order_wc,$server_url);
+        }
+
+    }
+
+}
+
+/* =================================================== Adding Status and Color icon ============================================================= */
+function register_erp_order_status()
+{
+    register_post_status( 		'wc-erp-processing', array(
+            'label'                     => 'Erp Processing',
+            'public'                    => true,
+            'exclude_from_search'       => false,
+            'show_in_admin_all_list'    => true,
+            'show_in_admin_status_list' => true,
+            'label_count'               => _n_noop( 'Erp Processing <span class="count">(%s)</span>', 'Erp Processing <span class="count">(%s)</span>' )
+        )
+    );
+}
+add_action( 'init', 'register_erp_order_status' );
+// Add to list of WC Order statuses
+function add_erp_processing_to_order_statuses( $order_statuses ) {
+
+    $new_order_statuses = array();
+    // add new order status after processing
+    foreach ( $order_statuses as $key => $status ) {
+
+        $new_order_statuses[ $key ] = $status;
+        if ( 'wc-processing' === $key )
+        {
+            $new_order_statuses['wc-erp-processing'] = 'Erp Processing';
+        }
+    }
+
+    return $new_order_statuses;
+}
+add_filter( 'wc_order_statuses', 'add_erp_processing_to_order_statuses' );
+
+// setting a wp test cron to run hourly
+// Colors for icons
+
+function menavip_delivery_only ($order,$order_wc,$url)
+{
+    $order_payment_method = strtoupper($order_wc->payment_method);
+    $data = array(
+        'username' 		 => get_option('mena_merchant_name'),
+        'password' 		 => get_option('mena_merchant_password'),
+        'method' 		 => 'create_orders',
+        'pickup_location'=> 'dubai',  //get_option('mena_pickup_location'),
+        'data' => array(
+            array(
+                'order_reference'  	=> 	  $order->ID,
+                'name' 				=> 	  $order_wc->shipping_first_name." ".$order_wc->shipping_last_name,
+                'email' 			=>    $order_wc->billing_email,
+                'phone_number'	 	=>    $order_wc->billing_phone,
+                'address' 			=>    $order_wc->get_shipping_address(),
+                'city' 				=>    $order_wc->shipping_city,
+                'payment_type' 		=>    $order_payment_method,
+                'amount' 			=>    $order_wc->get_total(),
+                'description'		=>	  time(),
+                'comments'			=>	  $order_wc->customer_message."   ".$order_wc->customer_note,
+                //'item'
+            )));
+    #echo '<pre>';
+    #print_r($data);exit;
+    $url = $url."client/api/";
+    $data_string = "args=" . json_encode($data);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    $results = curl_exec($ch);
+    $results = json_decode($results);
+    if ($results->status == "success")
+    {
+        // Change Status Here to ERP Processing
+        $order_wc->update_status( 'wc-erp-processing' );
+        // Create A custom field Airway bill number and update it
+
+        if ( ! update_post_meta ($order->ID, 'awb', $results->shipment_data ))
+        {
+            add_post_meta($order->ID, 'awb', $results->shipment_data, true );
+        }
+
+    }
+
+}
+
+function menavip_fulfil_delivery ($order,$order_wc,$products,$url)
+{
+    //print_r($product);
+    $item_list  = array ();
+
+    foreach ($products as $product)
+    {
+        $product_obj = new WC_Product($product['product_id']);
+        $sku = $product_obj->get_sku();
+        $n_product = array (
+            'client_ref'  	   	=> $order->ID,
+            'name' 		 	   	=> $product["name"],
+            'sku'		 	   	=> $sku,
+            'quantity' 	 	   	=> $product["qty"],
+            'merchant_details' 	=> array(
+                'mobile' 			=> trim(get_option('mena_merchant_phone_number')),
+                'phone'				=> trim(get_option('mena_merchant_phone_number')),
+                'name' 				=> get_option('mena_merchant_name'),
+                'address' 			=> ' NO '
+            ),
+            'COD' 				=> $order_wc->get_total_shipping(),// $order_wc->order_shipping
+            'price'		 		=> $product_obj->price ,//,$product_obj->get_regular_price()
+            'is_voucher' 		=> 'No'
+        );
+        array_push($item_list,$n_product);
+
+    }// product foreach loop
+
+    $datalist = array(array ('order' => array(
+        'items' => $item_list,
+        'details' => array(
+            'status' 				=> '',
+            'discount'			 	=> 0,
+            'grand_total'	    	=> $order_wc->get_total(),
+            'payment_method' 		=> strtolower($order_wc->payment_method),
+            'order_id' 				=> $order->ID,
+            'customer_firstname' 	=> $order_wc->shipping_first_name,
+            'customer_lastname' 	=> $order_wc->shipping_last_name,
+            'customer_mobile'		=> $order_wc->billing_phone,
+            'customer_email' 		=> $order_wc->billing_email,
+            'order_address' 		=> $order_wc->get_shipping_address()
+        )
+    )));
+
+    #echo '<pre>';
+    #print_r($datalist);
+    #exit;
+
+    $ERPdata 		= "ERPdata=".json_encode($datalist);
+    $erpuser		=  get_option('mena_merchant_name');	// "apifulfilment";
+    $erppassword	=  get_option('mena_merchant_password'); //"apifulfilment";
+    $merchant_name	=  get_option('erp_user_name');  //"API Test";//
+    $ch 			= curl_init();
+    $url 			= $url."client/gapicurl/";
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $ERPdata."&erpuser=".$erpuser."&erppassword=".$erppassword."&merchant_name=".$merchant_name);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec ($ch);
+    echo $response;
+    $results = json_decode($response,TRUE);
+    #echo '<pre>';
+    #print_r($results);
+    #exit;
+    if ($results["success"] == 1)
+    {
+        // Change Status Here to ERP Processing
+        $order_wc->update_status( 'wc-erp-processing' );
+        // Create A custom field Airway bill number and update it
+
+        if ( ! update_post_meta ($order->ID, 'awb', $results['response']['tracking_no'] ))
+        {
+            add_post_meta($order->ID, 'awb', $results['response']['tracking_no'], true );
+        }
+
+    }
+
+
+    curl_close ($ch);
+
+
+} // END of function
+
+
+
+function wc_order_status_styling() {
+    echo '<style>
+ .widefat .column-order_status mark.on-hold:after, .widefat .column-order_status mark.completed:after, 
+ .widefat .column-order_status mark.cancelled:after, .widefat .column-order_status mark.processing:after,
+ .widefat .column-order_status mark.erp-processing:after {
+ font-size: 2em;
+ }
+ /* Processing Ellipsis */
+ .widefat .column-order_status mark.processing:after {
+ color: #2529d7;
+ content: "\e011";
+ }
+ .erp-processing.tips:after {
+  color: #73a724 !important;
+}
+ /* On-Hold Dash */
+ .widefat .column-order_status mark.on-hold:after {
+ color: #555555;
+ content: "\e033";
+ }
+ /* Cancelled X */
+ .widefat .column-order_status mark.cancelled:after {
+ color: #d72525;
+ content: "\e013";
+ }
+ /* Completed Checkmark */
+ .widefat .column-order_status mark.completed:after {
+ color: #32d725;
+ content: "\e015";
+ }
+ 
+ .widefat .column-order_status mark.erp-processing {
+ color: #32d725 !important;
+ content: "\e015";
+ background-image: url("http://menavip.com/wp-content/uploads/2013/06/logo.png");
+ }
+ 
+ </style>';
+}
+add_action('admin_head', 'wc_order_status_styling');
